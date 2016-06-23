9bac890257b583265b96cc4a9c0125bb23c0abe6
Brett <brett@applogix.com>
2016-06-23 16:41:06 -0700

    Add nodemailer convenience methods

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,17 @@
 var crypto = require('crypto');
 var uuid = require('node-uuid');
+var jwt = require('jwt-simple');
 
-module.exports = {
+const time = {
+  seconds: ,
+  minutes: ,
+  hours: ,
+  days
+};
+
+auth = {
+
+
 
   authenticateApiRequest: () => {
 
@@ -13,7 +23,7 @@ module.exports = {
     //Create timestamp based UUID
     var key = uuid.v1();
 
-    //Generate a hash using 'sha256' algorithm, salting with ipAddress & using hex encoding
+    //Generate a hash using 'sha256' algorithm, salting with ipAddress & using hex encoding 
     key = crypto.createHash('sha256')
         .update(key)
         .update(ipAddress)
@@ -26,3 +36,27 @@ module.exports = {
   }
 
 }
+
+//Private methods
+//-------------------------------------
+//Generate the JWT token for the session
+genToken = (apiKeySecret) => {
+  var expires = expiresIn(1); //1 days
+  var token = jwt.encode({
+    exp: expires
+  }, require('../config/secret')());
+
+  return {
+    token: token,
+    expires: expires,
+    user: apiKeySecret
+  }
+}
+
+//Set expiry date for token
+expiresIn(numDays) {
+  var dateObj = new Date();
+  return dateObj.setDate(dateObj.getDate() + numDays);
+}
+
+module.exports = auth;
--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,9 +1,7 @@
 var nodemailer = require('nodemailer');
-var app = require('./server');
 
 //Configure SMTP Server details
-//-------------------------------------------------------
-var smtpTransport = nodemailer.createTransport("SMTP", {
+const smtpTransport = nodemailer.createTransport("SMTP", {
   service: 'Gmail',
   auth: {
     user: 'tandem4news@gmail.com',
@@ -11,51 +9,30 @@ var smtpTransport = nodemailer.createTransport("SMTP", {
   }
 });
 
-// var rand;
-// var mailOptions;
-// var host;
-// var link;
-//-------------------------------------------------------
-
-
-
-
-
-app.get('/login', (req, res) => {
-
-});
-
-app.get('/signup', (req, res) => {
-
-});
-
-app.get('/send', (req, res) => {
-  let rand = Math.floor((Math.random()*100000) + 71);
-  let host = req.get('host');
-  let link = req.protocol + '://' + req.get('host') + '/verify?id=' + rand;
-  let mailOptions = {
-    to: req.query.to,
-    subject: 'Plese verify your Email address',
-    html: 'Hello, <br> Please click on the link to verify your email.<br><a href=' + link + '>Click here to verify</a>'
+//Create standard mailOptions object
+createMessage = (emailTo, verifyLink) => {
+  var mailOptions = {
+    to: emailTo,
+    subject: 'Please verify your Email address',
+    html: 'Hello, <br> Please click on the link to verify your email.<br><a href=' + verifyLink + '>Click here to verify</a>'
   };
-  //TODO: remove
-  console.log(mailOptions);
+  return mailOptions;
+}
 
-  smtpTransport.sendMail(mailOption, (error, response) => {
-    if (err) {
-      console.log(error);
-      res.end('Error: ' + error);
+//Wrapper for sendMail method
+send = (mailOptions, callback) => {
+  smtpTransport.sendMail(mailOptions,(error, response) => {
+    if (error) {
+      callback(error);
     } else {
-      console.log('Message sent: ' + response.message);
-      res.end('Sent');
+      callback(null, response);
     }
   })
-});
+}
 
-app.get('/verify', (req, res) => {
-  //TODO: remove
-  console.log(req.protocol + '://' + req.get('host'));
-  if (req.protocol + '://' + req.get('host'));
+module.exports = {
+  createMessage: createMessage,
+  send: send
+}
 
-});
 
