c36cabc031491c04215f3635b27cd95688e272e8
Brett <brett@applogix.com>
2016-07-01 12:23:41 -0700

    Remove console.logs etc.

--- server/middleware/apiRateLimiter.js
+++ server/middleware/apiRateLimiter.js
@@ -22,20 +22,16 @@ var setRateLimitHeaders = (limit, remaining, rest) => {
   };
 };
 
-//Rate Limiting middleware function for api
+//Rate Limiting middleware function for api - use limited by user only (vs by user + endpoint)
 var rateLimiter = () => {
-  console.log("throttle called");
   return (req, res, next) => {
+    //Redis namespace for throttling datastructures Redis LIST)
     const nameSpace = 'apireq:';
-    var limit;
-    var remaining;
-    var rest;
-    //Limit API calls per user (vs per user per endpoint) - use user id if authenticated, else best guess ip address (anonymous user)
+    //Get user id if authenticated, else try and id anonymous users by ip address (best guess)
     var id = (req.user && req.user.id) ? req.user.id : req.ip || req.ips;
+    //Redis userKey for LIST datastructure
     var userKey = nameSpace + id;
-    console.log("REDIS userKey", userKey);
     client.llen(userKey, (error, replies) => {
-      console.log("REDIS LLEN error, replies:", error, replies);
       //Handle errors
       if (error) {
         console.log(error);
@@ -47,7 +43,6 @@ var rateLimiter = () => {
           .rpush(userKey, 1)
           .expire(userKey, 60) //Set TTL to 60 seconds
           .exec((error, replies) => {
-            console.log("REDIS MULTI error, replies:", error, replies);
             //Set Rate Limit custom headers
             res.set(setRateLimitHeaders(REQUEST_LIMIT_PER_MIN, REQUEST_LIMIT_PER_MIN - replies[0]));
             next();
@@ -55,7 +50,6 @@ var rateLimiter = () => {
       } else {
         //append to the list if it exists
         client.rpushx([userKey, 1], (error, replies) => {
-          console.log("REDIS RPUSHX error, replies:", error, replies);
           //Limit exceeded
           if (replies > REQUEST_LIMIT_PER_MIN) {
             //Set Rate Limit custom headers
@@ -71,28 +65,6 @@ var rateLimiter = () => {
         })
       }
     });
-
-    // X-Rate-Limit-Limit - The number of allowed requests in the current period
-    // X-Rate-Limit-Remaining - The number of remaining requests in the current period
-    // X-Rate-Limit-Reset - The number of seconds left in the current period
-
-        // FUNCTION LIMIT_API_CALL(ip)
-        // current = LLEN(ip)
-        // IF current > 10 THEN
-        //     ERROR "too many requests per second"
-        // ELSE
-        //     IF EXISTS(ip) == FALSE
-        //         MULTI
-        //             RPUSH(ip,ip)
-        //             EXPIRE(ip,1)
-        //         EXEC
-        //     ELSE
-        //         RPUSHX(ip,ip)
-        //     END
-        //     PERFORM_API_CALL()
-        // END
-
-      // });      
   }
 };
 
