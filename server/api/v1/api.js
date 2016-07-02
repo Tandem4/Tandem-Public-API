7f26633431ed22d5abd9100d2a0c1277e1b46b00
Brett <brett@applogix.com>
2016-07-01 17:11:18 -0700

    Add rate limit middleware to auth/back-end web portal routes

--- server/api/v1/api.js
+++ server/api/v1/api.js
@@ -5,6 +5,7 @@ var throttle = require('../../middleware/apiRateLimiter');
 * Routes relative to /api/v1/
 *********************************************/
 
+//API rate limiting middleware function
 router.use(throttle());
 //Mount all other routers for our api resources
 router.use('/trends', require('./trend/trendRoutes'));
